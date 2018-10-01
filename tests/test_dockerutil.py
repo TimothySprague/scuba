42b1eb62e4b493945656c4926ec1c195f26bfb06
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2018-09-30 21:03:06 -0400

    test: Remove nose.tools usage from test_dockerutil.py

--- tests/test_dockerutil.py
+++ tests/test_dockerutil.py
@@ -1,7 +1,6 @@
  # coding=utf-8
 from __future__ import print_function
 
-from nose.tools import *
 from .utils import *
 from unittest import TestCase
 try:
@@ -16,23 +15,25 @@ import scuba.dockerutil as uut
 class TestDockerutil(TestCase):
     def test_get_image_command_success(self):
         '''get_image_command works'''
-        assert_true(uut.get_image_command('debian:8.2'))
+        self.assertTrue(uut.get_image_command('debian:8.2'))
 
     def test_get_image_command_bad_image(self):
         '''get_image_command raises an exception for a bad image name'''
-        assert_raises(uut.DockerError, uut.get_image_command, 'nosuchimageZZZZZZZZ')
+        with self.assertRaises(uut.DockerError):
+            uut.get_image_command('nosuchimageZZZZZZZZ')
 
     def test_get_image_no_docker(self):
         '''get_image_command raises an exception if docker is not installed'''
 
         real_Popen = subprocess.Popen
         def mocked_popen(popen_args, *args, **kw):
-            assert_equal(popen_args[0], 'docker')
+            self.assertEqual(popen_args[0], 'docker')
             popen_args[0] = 'dockerZZZZ'
             return real_Popen(popen_args, *args, **kw)
 
         with mock.patch('subprocess.Popen', side_effect=mocked_popen) as popen_mock:
-            assert_raises(uut.DockerError, uut.get_image_command, 'n/a')
+            with self.assertRaises(uut.DockerError):
+                uut.get_image_command('n/a')
 
     def test__get_image_command__pulls_image_if_missing(self):
         '''get_image_command pulls an image if missing'''
@@ -60,19 +61,19 @@ class TestDockerutil(TestCase):
 
 
     def test_make_vol_opt_no_opts(self):
-        assert_equal(
+        self.assertEqual(
                 uut.make_vol_opt('/hostdir', '/contdir'),
                 '--volume=/hostdir:/contdir'
                 )
 
     def test_make_vol_opt_one_opt(self):
-        assert_equal(
+        self.assertEqual(
                 uut.make_vol_opt('/hostdir', '/contdir', 'ro'),
                 '--volume=/hostdir:/contdir:ro'
                 )
 
     def test_make_vol_opt_multi_opts(self):
-        assert_equal(
+        self.assertEqual(
                 uut.make_vol_opt('/hostdir', '/contdir', ['ro', 'z']),
                 '--volume=/hostdir:/contdir:ro,z'
                 )
